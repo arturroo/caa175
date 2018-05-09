382ce2cff4b1f5e303f58af16a1bd944bffafbba
Fejklowicz Artur <A.Fejklowicz@tvn.pl>
2018-05-09 12:59:50 +0200

    working scalademo retail

--- scala/scalademo/retail/src/main/scala/orderRevenue.scala
+++ scala/scalademo/retail/src/main/scala/orderRevenue.scala
@@ -2,10 +2,11 @@ import scala.io.Source
 
 object OrderRevenue {
 	def main(args: Array[String]) = {
+		val orderId = args(1).toInt
 		val orderItemsFile = "/mnt/c/Users/admin/Documents/stash/dgadiraju/data/retail_db/order_items/part-00000"
 		val orderItems = Source.fromFile(orderItemsFile).getLines // Iterator
 		val orderRevenue = orderItems
-			.filter(_.split(",")(1).toInt == 2)
+			.filter(_.split(",")(1).toInt == orderId)
 			.map(_.split(",")(4).toDouble)
 			.reduce(_+_)
 		println(orderRevenue)
